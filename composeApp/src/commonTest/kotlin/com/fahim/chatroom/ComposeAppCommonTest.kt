09daf1be72700edda87a9c98a79eb8d8ce793219
ifahimkhan <khan.fahim95@hotmail.com>
2026-05-12 23:21:16 +0530

    Initial commit

---
+++ composeApp/src/commonTest/kotlin/com/fahim/chatroom/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.fahim.chatroom
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/fahim/chatroom/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.fahim.chatroom
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
